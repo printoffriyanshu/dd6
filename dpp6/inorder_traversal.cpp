1ec54e95ebe4f56d079d09f628700fc1a0a36eb2
Riyanshu Dubey <indu454567@gmail.com>
2023-03-19 18:57:23 +0530

    dpp6 commit

---
+++ dpp6/inorder_traversal.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define whl(tt) while(tt--)
+struct Node
+{
+    int data;
+    struct Node *left,*right;
+};
+Node* newNode(int data){
+    Node* temp= new Node;
+    temp->data=data;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+void inorder(Node* root){
+    if (root==NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    
+}
+
+int main(){
+   struct Node *root=newNode(1);
+   root->left = newNode(2);
+   root->right = newNode(3);
+   root->left->left = newNode(4);
+   root->left->right=newNode(5);
+
+   inorder(root);
+  
+return 0;
+}
